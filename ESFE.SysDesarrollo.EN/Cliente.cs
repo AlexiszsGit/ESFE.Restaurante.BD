f1277324e2dcfd874784f851e48a87cd0306d935
Alexis cruz <Cruzzalexis14@gmail.com>
2026-08-10 19:19:30 +0200

    cracion de clase cliente

---
+++ ESFE.SysDesarrollo.EN/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.Restaurante.EN
+{
+    public class Cliente
+    {
+     public int IdCliente  { get; set; }
+     public string NombreAlexis  { get; set; }
+     public string Email { get; set; 
+    }
+}

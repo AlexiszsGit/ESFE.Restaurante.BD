b6a9933aeed3c3ef82d9f31de2c713e5e36bbd8b
Alexis cruz <Cruzzalexis14@gmail.com>
2026-08-11 00:27:00 +0200

    ya estan las primeras 5 tablas

--- ESFE.SysDesarrollo.EN/Cliente.cs
+++ ESFE.SysDesarrollo.EN/Cliente.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ESFE.Restaurante.EN
+namespace ESFE.RestauranteBD.EN
 {
     public class Cliente
     {
-     public int IdCliente  { get; set; }
-     public string NombreAlexis  { get; set; }
-     public string Email { get; set; }
-        hola a todos 
+        public string IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
     }
 }

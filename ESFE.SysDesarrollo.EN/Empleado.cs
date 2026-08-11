a7558975b82c8069ef3414d704e9b8ac18b46317
Fernando <Fernando@DESKTOP-LQGPM4B>
2026-08-11 08:48:51 -0600

    esta es mi tabla

---
+++ ESFE.SysDesarrollo.EN/Empleado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.RestauranteBD.EN
+{
+    public class Empleado
+    {
+      public int IdEmpleado { get; set; }
+
+    }
+}

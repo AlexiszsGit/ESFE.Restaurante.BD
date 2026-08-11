41e44a2fd259856bae6ff62a73601dfd6f341c15
Alexis cruz <Cruzzalexis14@gmail.com>
2026-08-11 18:08:26 +0200

    actualizacion

--- ESFE.SysDesarrollo.EN/DetallePedidos.cs
+++ ESFE.SysDesarrollo.EN/DetallePedidos.cs
@@ -6,7 +6,7 @@ namespace ESFE.RestauranteBD.EN
 {
     public class DetallePedidos
     {
-        public string IdDetalle { get; set; }
+        public int IdDetalle { get; set; }
         public string? IdPedido { get; set; }
         public int Cantidad { get; set; }
     }

7101a16b8fb882363dbbd2d7a8e192529e916fb3
Fernando <Fernando@DESKTOP-LQGPM4B>
2026-09-08 09:18:42 -0600

    controller listos

--- ESFE.SysDesarrollo.EN/Cliente.cs
+++ ESFE.SysDesarrollo.EN/Cliente.cs
@@ -9,5 +9,6 @@ namespace ESFE.RestauranteBD.EN
         public string IdCliente { get; set; }
         public string Nombre { get; set; }
         public string Telefono { get; set; }
+       // 123
     }
 }

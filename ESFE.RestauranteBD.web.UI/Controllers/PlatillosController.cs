6c60db6957717e696965a6024c82187512a9cbd1
Fernando <Fernando@DESKTOP-LQGPM4B>
2026-09-07 21:29:20 -0600

    123

---
+++ ESFE.RestauranteBD.web.UI/Controllers/PlatillosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE.RestauranteBD.web.UI.Controllers
+{
+    public class PlatillosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

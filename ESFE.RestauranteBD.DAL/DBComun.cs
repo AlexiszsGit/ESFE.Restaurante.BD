db1f4170b9d4c5ae404150acb1fc40542a4156e3
Fernando <Fernando@DESKTOP-LQGPM4B>
2026-09-08 09:23:53 -0600

    controllers listos

--- ESFE.RestauranteBD.DAL/DBComun.cs
+++ ESFE.RestauranteBD.DAL/DBComun.cs
@@ -11,9 +11,9 @@ namespace ESFE.RestauranteBD.DAL
         //Alexis
         //public const string _stringCnn = @"Server=(localdb)\MSSQLLocalDB;Database=RestauranteBD;Integrated Security=True;TrustServerCertificate=True;";
         //Pinti 
-       // public const string _stringCnn = @"Server=.\SQLEXPRESS2;Database=RestauranteBD;Integrated Security=True;TrustServerCertificate=True;";
+       public const string _stringCnn = @"Server=.\SQLEXPRESS2;Database=RestauranteBD;Integrated Security=True;TrustServerCertificate=True;";
         //Henri
-       public const string _stringCnn = @"Server=HENRI\SQLEXPRESS;Database=RestauranteBD;Integrated Security=True;TrustServerCertificate=True;";
+      // public const string _stringCnn = @"Server=HENRI\SQLEXPRESS;Database=RestauranteBD;Integrated Security=True;TrustServerCertificate=True;";
 
         /// <summary>
         /// Metodo para obtener base de datos.

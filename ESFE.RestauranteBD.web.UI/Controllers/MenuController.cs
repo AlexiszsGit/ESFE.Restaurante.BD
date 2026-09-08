b7b5e93b939962958d01ce86629aca655e4652c4
Alexis cruz <Cruzzalexis14@gmail.com>
2026-09-08 15:40:27 +0200

    u

---
+++ ESFE.RestauranteBD.web.UI/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE.RestauranteBD.web.UI.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+

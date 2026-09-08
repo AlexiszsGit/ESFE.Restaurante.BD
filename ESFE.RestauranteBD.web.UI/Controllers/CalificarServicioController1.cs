abb70b7b453d9bf1dc1c1120f346e78a6beaaf5f
Dell <Dell@DESKTOP-B0LN7CL>
2026-09-08 09:25:14 -0600

    controles terminados

---
+++ ESFE.RestauranteBD.web.UI/Controllers/CalificarServicioController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE.RestauranteBD.web.UI.Controllers
+{
+    public class CalificarServicioController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ESFE.RestauranteBD.web.UI/Controllers/PedidoListoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE.RestauranteBD.web.UI.Controllers
+{
+    public class PedidoListoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
